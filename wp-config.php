450247ead139f88be3663cea750ff123a06ddd17
cod3mau5 <mauri.bmxxx@gmail.com>
2021-11-04 20:47:27 -0700

    wp-config changed from server

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'aonecabo');
+define('DB_NAME', 'aonecabo_web');
+
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'aonecabo_web');
+
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'Amarillo77');
+
 /** MySQL hostname */
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'mysql.aonecabodeluxetransportation.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
